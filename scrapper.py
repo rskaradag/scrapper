91e7c5cf329e5bc747a4c676ed1d726156691379
Selçuk Karadağ <r.selcukkaradag@gmail.com>
2021-01-06 10:55:06 +0300

    finalize

--- scrapper.py
+++ scrapper.py
@@ -1,56 +1,86 @@
 import requests
-import urllib.request
 import time
 import json
-import csv
+import xlsxwriter
+import decimal
 import subprocess, sys,os
+from datetime import date
+from bs4 import BeautifulSoup
+
+import email, smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 #response = requests.get('https://www.gratis.com/ccstoreui/v1/search?N=3790920594&Nrpp=50&No=0&Nr=AND(product.active%3A1%2CNOT(sku.listPrice%3A0.000000))&Ns=')
 #response = requests.get('https://www.gratis.com/ccstoreui/v1/search?N=3790920594&Nrpp=50&No=50&Nr=AND(product.active%3A1%2CNOT(sku.listPrice%3A0.000000))&Ns=')
 
-#3207 product
-
-
-
-
-
-url=""
-
 def fetch_watsons():
-	#print(os.getcwd())
-	#f = open("cmd", "r")
-	#line=f.read()
-	#print(line)
-	#p = subprocess.Popen(["C:\\Program Files\\Git\\git-bash.exe", 
-	            #  line], 
-	            #  stdout=sys.stdout)
-	#print(p.communicate())
+	url = "https://www.watsons.com.tr/c/makyaj-281?pagenumber="
+	#64
+	output=[]
+	line=[]
+	for i in range(1,5):
+		tmp_url=url + str(i)
+		print(tmp_url)
+		response = requests.get(tmp_url)
+		data=response.content
 
-	url = "https://gandalf.segmentify.com//add/events/v1.json?apiKey=7a149359-e360-4669-aa11-a2d8a9ecf0a6"
+		soup = BeautifulSoup(data, 'html.parser', fromEncoding='utf-8')
 
-	payload = "{\"async\": \"true\",\r\n\"browser\": \"Chrome\",\r\n\"category\": \"Category Page\",\r\n\"currency\": \"\",\r\n\"device\": \"PC\",\r\n\"email\": \"\",\r\n\"lang\": \"TR\",\r\n\"name\": \"PAGE_VIEW\",\r\n\"nextPage\": false,\r\n\"noProcess\": false,\r\n\"os\": \"Windows\",\r\n\"osversion\": \"10\",\r\n\"params\":{\"gender\":\"__ANY__\"},\r\n\"pageUrl\": \"https://www.watsons.com.tr/c/makyaj-281?pagenumber=2\",\r\n\"recommendIds\": [],\r\n\"referrer\": \"\",\r\n\"region\": \"\",\r\n\"sessionId\": \"7298412948190920704\",\r\n\"subCategory\": \"Makyaj\",\r\n\"testMode\": \"false\",\r\n\"tryCount\": 0,\r\n\"type\":\"widget-view\",\r\n\"tz\": \"-180\",\r\n\"userId\":\"2380481611291172865\",\r\n\"userAgent\": \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36\",\r\n\"ft\":\"2020.10.29 13:04:11.792\"\r\n}"
-	headers = {
-	  'Content-Type': 'text/plain',
-	  'X-Sfy-Api-Key': '7a149359-e360-4669-aa11-a2d8a9ecf0a6',
-	  'Accept': '*/*',
-	  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-	  'Origin': 'https://www.watsons.com.tr',
-	  'Sec-Fetch-Site': 'cross-site',
-	  'Sec-Fetch-Mode': 'cors',
-	  'Sec-Fetch-Dest': 'empty',
-	  'Referer': 'https://www.watsons.com.tr/',
-	  'Accept-Encoding': 'gzip, deflate, br',
-	  'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7'
-	}
+		products = soup.select(".product-box-container.border.rounded.p-1.pb-4.h-100")
+		for item in products:
+			_barcode="-"
+			_productName=item.select(".productbox-desc.text-left.mb-1")[0].text.strip().split("\n")[2]
+			#print("Brand: " + item.select(".productbox-desc.text-left.mb-1")[0].text.strip().split("\n")[0])
+			#print("Model: " + item.select(".productbox-desc.text-left.mb-1")[0].text.strip().split("\n")[2])
 
-	response = requests.request("POST", url, headers=headers, data = payload)
-	print(response.headers['Content-Type'])
+			print(_productName)
+			if item.select(".list-inline-item.product-box-old-price.text-site-medium-gray.roboto-regular"):
+				_campaign="Var"
+				_listPrice=item.select(".list-inline-item.product-box-old-price.text-site-medium-gray.roboto-regular")[0].text.strip().split(" ")[0]
+				_discount="-"
+				_salesPrice=item.select(".list-inline-item.product-box-price.text-site-pink.roboto-medium")[0].text.strip().split(" ")[0]
+			else:
+				_campaign="Yok"
+				_listPrice=item.select(".list-inline-item.product-box-price.text-site-pink.roboto-medium")[0].text.strip().split(" ")[0]
+				_discount="-"
+				_salesPrice="-"
+
+			if item.select(".product-box-has-no-stock.position-absolute.l-50.t-50.text-center.rubik-medium.text-white.p-2.rounded.border.border-white"):
+				_stock=item.select(".static-text")[0].text.strip()
+			else:
+				_stock="STOKTA"
+
+			line.append(_barcode)
+			line.append(_campaign)
+			line.append(_productName)
+			line.append(_listPrice)
+			line.append(_discount)
+			line.append(_salesPrice)
+			line.append(_stock)
+
+			[x.encode('UTF8') for x in line]
+
+			output.append(line)
+			line=[]
+
+	with xlsxwriter.Workbook('WatsonsDailyReport '+date.today().strftime("%d-%m-%Y")+'.xlsx') as workbook:
+		workbook.encoding="utf-8"
+		worksheet = workbook.add_worksheet()
+		worksheet.write_row(0,0,["Barkod","Kampanya", "Urun Adi","Liste Fiyati","Indirim","Satis Fiyati","Stok"])
+		for row_num, data in enumerate(output):
+			row_num +=1
+			worksheet.write_row(row_num, 0, data)
 
 def fetch_gratis():
-
 	output=[]
 	line=[]
 	k=0
+	#3300
 	for i in range(100,3300,100):
 
 		print(str(i))
@@ -63,41 +93,89 @@ def fetch_gratis():
 				r.encoding
 				d_product = json.loads(r.content)
 				line.append(str(product["records"][0]["attributes"]["sku.grm_DynamicSearchText3"][0]) if "sku.grm_DynamicSearchText3" in product["records"][0]["attributes"] else "")
-				line.append(str(product["records"][0]["attributes"]["product.id"][0]))
 				line.append(str(product["records"][0]["attributes"]["sku.gr_EanUpc"][0]))
-				line.append(str(product["records"][0]["attributes"]["sku.displayName"][0]))
-				line.append(d_product["items"][0]["listPrices"]["TurkeyPriceGroup"])
-				line.append(str(product["records"][0]["attributes"]["sku.grm_Campaignsearch"][0]))
-				line.append(d_product["items"][0]["listPrices"]["loyaltyTurkeyPriceGroup"])
+				line.append(product["records"][0]["attributes"]["sku.displayName"][0])
+				line.append(decimal.Decimal(d_product["items"][0]["listPrices"]["TurkeyPriceGroup"]))
+
+				_discount=str(round(decimal.Decimal(d_product["items"][0]["listPrices"]["loyaltyTurkeyPriceGroup"])/(decimal.Decimal(d_product["items"][0]["listPrices"]["TurkeyPriceGroup"]))*100,1))+"%"
+
+				#line.append(str(product["records"][0]["attributes"]["sku.grm_Campaignsearch"][0]))
+				line.append(_discount)
+				line.append(decimal.Decimal(d_product["items"][0]["listPrices"]["loyaltyTurkeyPriceGroup"]))
 
 				#print(str(product["records"][0]["attributes"]["sku.grm_DynamicSearchText3"][0]) +str(product["records"][0]["attributes"]["product.id"][0])+ str(product["records"][0]["attributes"]["sku.displayName"][0]) + " - " + str(d_product["items"][0]["listPrices"]["TurkeyPriceGroup"]) + " - " + str(product["records"][0]["attributes"]["sku.grm_Campaignsearch"][0]) + "--->" + str(d_product["items"][0]["listPrices"]["loyaltyTurkeyPriceGroup"]))
 			else:
 				r.encoding
-				line.append(" ")
-				line.append(str(product["records"][0]["attributes"]["product.id"][0]))
+				line.append("-")
 				line.append(str(product["records"][0]["attributes"]["sku.gr_EanUpc"][0]))
-				line.append(str(product["records"][0]["attributes"]["sku.displayName"][0]))
-				line.append(product["records"][0]["attributes"]["sku.salePrice"][0])
-				line.append(" ")
-				line.append(0)
+				line.append(product["records"][0]["attributes"]["sku.displayName"][0])
+				line.append(decimal.Decimal(product["records"][0]["attributes"]["sku.salePrice"][0]))
+				line.append("-")
+				line.append("-")
 				#print(str(product["records"][0]["attributes"]["sku.displayName"][0]) +str(product["records"][0]["attributes"]["product.id"][0])+ "--->" + str(product["records"][0]["attributes"]["sku.salePrice"][0]))
 
 			output.append(line)
 			line=[]
 			k=k+1
 
+	with xlsxwriter.Workbook('GratisDailyReport '+date.today().strftime("%d-%m-%Y")+'.xlsx') as workbook:
+		workbook.encoding="utf-8"
+		worksheet = workbook.add_worksheet()
+		worksheet.write_row(0,0,["Kampanya","Barkod", "Urun Adi","Liste Fiyati","Indirim","Satis Fiyati"])
+		for row_num, data in enumerate(output):
+			row_num +=1
+			worksheet.write_row(row_num, 0, data)
 
-	with open('out.csv', 'w', newline='',encoding='utf-8') as file:
-		writer = csv.writer(file)
-		writer.writerow(["Kampanya", "ProductId","Barkod", "Urun Adi","Liste Fiyati","Indirim","Satis Fiyati"])
-		for item in output:
-			#print(item)
-			writer.writerow(item)
+def send_email():
+	subject = "Gratis & Watsons Daily Report "+date.today().strftime("%d-%m-%Y")
+	body = "This is an email with attachment sent from data collector service by RSK"
+	sender_email = "automationinforsk@gmail.com"
+	receiver_email = "gozde.uysal@eveshop.com.tr"
+	password = "Gzdeuysal2"
+
+	# Create a multipart message and set headers
+	message = MIMEMultipart()
+	message["From"] = sender_email
+	message["To"] = receiver_email
+	message["Subject"] = subject
+	#message["Bcc"] = receiver_email  # Recommended for mass emails
+
+	# Add body to email
+	message.attach(MIMEText(body, "plain"))
+
+	filename = 'GratisDailyReport '+date.today().strftime("%d-%m-%Y")+'.xlsx' # In same directory as script
+
+	# Open PDF file in binary mode
+	with open(filename, "rb") as attachment:
+	    # Add file as application/octet-stream
+	    # Email client can usually download this automatically as attachment
+		part = MIMEBase("application", "octet-stream")
+		part.set_payload(attachment.read())
+
+	# Encode file in ASCII characters to send by email    
+	encoders.encode_base64(part)
+
+	# Add header as key/value pair to attachment part
+	part.add_header(
+	    "Content-Disposition",
+	    f"attachment; filename= {filename}",
+	)
+
+	# Add attachment to message and convert message to string
+	message.attach(part)
+	text = message.as_string()
+
+	# Log in to server using secure context and send email
+	context = ssl.create_default_context()
+	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+		server.login(sender_email, password)
+		server.sendmail(sender_email, receiver_email, text)
 
 def main():
 	print("hello")
 	fetch_watsons()
 	fetch_gratis()
+	send_email()
 
 if __name__ == '__main__':
 	main()
--- test.py
+++ test.py
@@ -1,34 +1,35 @@
+from bs4 import BeautifulSoup
 import requests
-import time
-import json 
 
-url = "https://gandalf.segmentify.com//add/events/v1.json?apiKey=7a149359-e360-4669-aa11-a2d8a9ecf0a6"
+#https://www.watsons.com.tr/c/makyaj-281?pagenumber=2
 
-payload = "{\"async\": \"true\",\r\n\"browser\": \"Chrome\",\r\n\"category\": \"Category Page\",\r\n\"currency\": \"\",\r\n\"device\": \"PC\",\r\n\"email\": \"\",\r\n\"lang\": \"TR\",\r\n\"name\": \"PAGE_VIEW\",\r\n\"nextPage\": false,\r\n\"noProcess\": false,\r\n\"os\": \"Windows\",\r\n\"osversion\": \"10\",\r\n\"params\":{\"gender\":\"__ANY__\"},\r\n\"pageUrl\": \"https://www.watsons.com.tr/c/makyaj-281?pagenumber=2\",\r\n\"recommendIds\": [],\r\n\"referrer\": \"\",\r\n\"region\": \"\",\r\n\"sessionId\": \"7298412948190920704\",\r\n\"subCategory\": \"Makyaj\",\r\n\"testMode\": \"false\",\r\n\"tryCount\": 0,\r\n\"type\":\"widget-view\",\r\n\"tz\": \"-180\",\r\n\"userId\":\"2380481611291172865\",\r\n\"userAgent\": \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36\",\r\n\"ft\":\"2020.10.29 13:04:11.792\"\r\n}"
-headers = {
-  'Content-Type': 'text/plain',
-  'X-Sfy-Api-Key': '7a149359-e360-4669-aa11-a2d8a9ecf0a6',
-  'Accept': '*/*',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-  'Origin': 'https://www.watsons.com.tr',
-  'Sec-Fetch-Site': 'cross-site',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Dest': 'empty',
-  'Referer': 'https://www.watsons.com.tr/',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-  'Connection': 'keep-alive',
-  'Host': 'gandalf.segmentify.com'
-}
+url = "https://www.watsons.com.tr/c/makyaj-281?pagenumber="
+#64
 
-response = requests.request("POST", url, headers=headers, data = payload,stream=True)
+for i in range(1,65):
+  tmp_url=url + str(i)
+  print(tmp_url)
+  response = requests.get(tmp_url)
 
-print(response.headers)
-print(response.status_code)
+  data=response.content
+
+  soup = BeautifulSoup(data, 'html.parser')
+
+  products = soup.select(".product-box-container.border.rounded.p-1.pb-4.h-100")
+  for item in products:
+    #print("Brand: " + item.select(".productbox-desc.text-left.mb-1")[0].text.strip().split("\n")[0])
+    print("Model: " + item.select(".productbox-desc.text-left.mb-1")[0].text.strip().split("\n")[2])
+    if item.select(".mb-1"):
+      print("Kampanya var")
+    if item.select(".list-inline-item.product-box-old-price.text-site-medium-gray.roboto-regular"):
+      print("Old Price: " + item.select(".list-inline-item.product-box-old-price.text-site-medium-gray.roboto-regular")[0].text.strip())
+    #print("Price: " + item.select(".list-inline-item.product-box-price.text-site-pink.roboto-medium")[0].text.strip())
+    print("-----------------------------------------------")
 
 
-#with open("out.txt", 'wb') as fd:
-#  for chunk in response.iter_content(chunk_size=128):
-#    fd.write(chunk)
 
-time.sleep(5)
+
+
+
+
+#parse_select(data)
